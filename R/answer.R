8d14faeed1aa77bd42d900eb22bf0c24c367be70
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-03-08 12:23:24 +1100

    Add some clozure answer types for demo and testing

---
+++ R/answer.R
@@ -0,0 +1,37 @@
+# Valid answer types: multichoice|truefalse|shortanswer|matching|cloze|essay|numerical|description
+
+#' @export
+answer_shortanswer <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+  sprintf(
+    "{%i:SHORTANSWER:%s}",
+    weight,
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+  )
+}
+
+#' @export
+answer_multichoice <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+  sprintf(
+    "{%i:MULTIRESPONSE:%s}",
+    weight,
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+  )
+}
+#' @export
+
+answer_singlechoice <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+  sprintf(
+    "{%i:MULTICHOICE:%s}",
+    weight,
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+  )
+}
+
+#' @export
+answer_numerical <- function(correct, weight = 1, tolerance = 0, correct_feedback = sprintf("Correct, %f is the correct answer.", correct)) {
+  # Add alternative solutions / thresholds
+  sprintf(
+    "{%i:NUMERICAL:=%f:%f#%s}",
+    weight, correct, tolerance, correct_feedback
+  )
+}
