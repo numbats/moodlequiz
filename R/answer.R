6cc284bcfd2c1f409708fd2db6c637b06b3aa241
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-05-02 17:06:47 +1000

    Add alternative formats for short answer and single/multiple responses

--- R/answer.R
+++ R/answer.R
@@ -1,29 +1,47 @@
 # Valid answer types: multichoice|truefalse|shortanswer|matching|cloze|essay|numerical|description
 
 #' @export
-answer_shortanswer <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+answer_shortanswer <- function(
+    options, weight = max(options),
+    feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct")),
+    case_sensitive = FALSE) {
   sprintf(
-    "`{%i:SHORTANSWER:%s}`{=html}",
+    "`{%i:SHORTANSWER%s:%s}`{=html}",
     weight,
-    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~"),
+    if(case_sensitive) "_C" else ""
   )
 }
 
 #' @export
-answer_multichoice <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+answer_multichoice <- function(
+    options, weight = max(options),
+    feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct")),
+    type = c("vertical", "horizontal"),
+    shuffle = FALSE) {
   sprintf(
-    "`{%i:MULTIRESPONSE:%s}`{=html}",
+    "`{%i:MULTIRESPONSE%s%s%s:%s}`{=html}",
     weight,
-    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~"),
+    if(shuffle || type != "vertical") "_" else "",
+    switch(type, vertical = "", horizontal = "H"),
+    if(shuffle) "S" else ""
   )
 }
 #' @export
 
-answer_singlechoice <- function(options, weight = max(options), feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct"))) {
+answer_singlechoice <- function(
+    options, weight = max(options),
+    feedback = ifelse(options == weight, "Correct", ifelse(options == 0, "Incorrect", "Partially correct")),
+    type = c("dropdown", "vertical", "horizontal"),
+    shuffle = FALSE) {
   sprintf(
     "`{%i:MULTICHOICE:%s}`{=html}",
     weight,
-    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~")
+    paste0("%", options/weight*100, "%", names(options), "#", feedback, collapse = "~"),
+    if(shuffle || type != "dropdown") "_" else "",
+    switch(type, dropdown = "", vertical = "V", horizontal = "H"),
+    if(shuffle) "S" else ""
   )
 }
 
