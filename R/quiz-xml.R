3a1572b6462f4019e0f722db4b4b4eb0026d953e
Mitchell O'Hara-Wild <mail@mitchelloharawild.com>
2025-01-16 19:26:44 +1100

    Document moodlequiz_xml

--- R/quiz-xml.R
+++ R/quiz-xml.R
@@ -78,6 +78,32 @@ moodlequiz <- function(replicates = 1L,
   )
 }
 
+#' Generate Moodle Quiz XML Output for R Markdown
+#'
+#' This function is intended for internal generation of the XML output.
+#' It is strongly recommended that you use the `[moodlequiz::moodlequiz()]`
+#' output format, which provides a higher-level interface for creating
+#' Moodle-compatible quizzes.
+#'
+#' @param replicate A character string specifying the how many replications of the quiz should be produced.
+#' @param self_contained Logical. If `TRUE`, the output document will be self-contained,
+#'   embedding resources directly into the file. Defaults to `TRUE`.
+#' @param extra_dependencies Additional dependencies to include in the output. These can be
+#'   specified as an `html_dependency` object or a list of such objects.
+#' @param theme A character string specifying the theme for the output. This can be a standard
+#'   theme name or a custom CSS file.
+#' @param includes Additional content to include in the output. This should be a list of
+#'   named elements, such as `in_header`, `before_body`, and `after_body`.
+#' @param lib_dir Directory to copy library files for the output. If `NULL`, no library files
+#'   are copied.
+#' @param md_extensions A character string specifying Markdown extensions to be passed to Pandoc.
+#' @param pandoc_args Additional arguments to pass to Pandoc.
+#' @param ... Additional arguments passed to [`bookdown::html_document2()`]
+#'
+#' @return An R Markdown output format object.
+#'
+#' @seealso [moodlequiz::moodlequiz()]
+#'
 #' @keywords internal
 #' @export
 moodlequiz_xml <- function(replicate = "",
